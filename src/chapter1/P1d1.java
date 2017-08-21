8e7ab4aa48ca0a91fa26065fd9d3a5c317609a0a
Youssef Salama <youssefhanii@gmail.com>
2017-08-21 10:30:53 +0200

    second commit

--- src/chapter1/P1d1.java
+++ src/chapter1/P1d1.java
@@ -1,5 +1,33 @@
 package chapter1;
-//
-public class P1d1 {
 
+import java.util.HashSet;
+
+//implement an algorithm to detect if a string has all unique characters
+public class P1d1 {
+	public static void main(String[] args) {
+		System.out.println(hasAllUniqueCharacters("jcwoerijcw98erjc9w83jv"));//false
+		System.out.println(hasAllUniqueCharacters("andkwet"));//true
+		
+		
+
+	}
+	//this function uses a HashSet
+	private static boolean hasAllUniqueCharacters(String str) {
+		//edge case
+		if(str == null || str == "") {
+			return true;
+		}
+		char[] strChared = str.toCharArray();
+		HashSet<Character> hs = new HashSet<>();
+		//Iteration
+		for (int i = 0; i < strChared.length; i++) {
+			char c = strChared[i];
+			if(hs.contains(c)) {
+				return false;
+			}
+			hs.add(c);
+			
+		}
+		return true;
+	}
 }
